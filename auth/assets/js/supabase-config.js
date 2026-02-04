7ec8681fe8c1a41d39f6bc42fdff325bf08cf856
itsmepskiran <itsmepskiran84@gmail.com>
2026-02-04 13:38:56 +0530

    fixing supabase issues

--- auth/assets/js/supabase-config.js
+++ auth/assets/js/supabase-config.js
@@ -2,45 +2,40 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const SUPABASE_URL = 'https://lgmvbmbzxsqrcclaynuh.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxnbXZibWJ6eHNxcmNjbGF5bnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxMzgxODQsImV4cCI6MjA4NDcxNDE4NH0.F63Fe9zFBuYni-qxZXwIzJNgCvM-rDxAi5_gFDGrXAM'
-
+const storage = {
+  getitem: (key) => {
+    const value = localStorage.getItem(key);
+    console.log('getItem', key, value);
+    return value;
+  },
+  setitem: (key, value) => {
+    console.log('setItem', key, value);
+    localStorage.setItem(key, value);
+  },
+  removeitem: (key) => {
+    console.log('removeItem', key);
+    localStorage.removeItem(key);
+  }
+}
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,     // Required for PKCE
     flowType: 'pkce',             // Required for cookie-based auth
-    storage:{
-      getitem: (key ) => {
-        return localStorage.getItem(key);
-      },
-      setitem: (key, value) => {
-        localStorage.setItem(key, value);
-      },
-      removeitem: (key) => {
-        localStorage.removeItem(key);
-      }
-    },
+    storage: storage,
     // ⭐ Shared cookie across ALL Skreenit subdomains
     cookieOptions: {
       name: 'sb-access-token',
       lifetime: 60*60*24*7, // 7 days
       domain: '.skreenit.com',
       path: '/',
-      sameSite: 'lax',
-    },
-  },
-});
+      sameSite: 'lax'
+    }
+  }
+})
 
 // Keep only role in localStorage
 supabase.auth.onAuthStateChange((event, session) => {
-  if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
-    const role = session?.user?.user_metadata?.role;
-    if (role) {
-      localStorage.setItem('skreenit_role', role);
-    }
-  }
-
-  if (event === 'SIGNED_OUT') {
-    localStorage.removeItem('skreenit_role');
-  }
-});
+  console.log('Auth state change:', event, session)
+})
